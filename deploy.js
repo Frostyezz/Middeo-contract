652273fdfe5a3b46293c6d82da569facf367ab64
Robert Moraru <robert.moraru2012@yahoo.com>
2022-07-25 20:55:16 +0300

    added commands

--- deploy.js
+++ deploy.js
@@ -18,7 +18,7 @@ const deploy = async () => {
     .send({ gas: "1000000", from: accounts[0] });
 
   console.log("Contract deployed to", result.options.address);
-  console.log(JSON.parse(interface));
+  console.log(interface);
 
   provider.engine.stop();
 };
