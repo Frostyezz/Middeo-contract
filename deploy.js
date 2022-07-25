d32285576fa49fe0326a75c47a7e3da278da5a8b
Robert Moraru <robert.moraru2012@yahoo.com>
2022-07-25 20:31:39 +0300

    added deployment script

---
+++ deploy.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  process.env.MNEMONIC,
+  process.env.INFURA_ENDPOINT
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  console.log(JSON.parse(interface));
+
+  provider.engine.stop();
+};
+deploy();
